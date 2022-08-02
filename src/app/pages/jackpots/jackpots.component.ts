b2935c132f396df8ea7011a93562b6df226a0a7d
Firas Chabchoub <firaschabchoub@hotmail.com>
2022-08-02 17:12:20 +0100

    refactor: working on the jackpots

--- src/app/pages/jackpots/jackpots.component.ts
+++ src/app/pages/jackpots/jackpots.component.ts
@@ -9,11 +9,28 @@ import { Component, OnInit } from '@angular/core';
 export class JackpotsComponent implements OnInit {
 
   jackpots!: any[]
+  games!: any[]
+  list!: any[]
 
   constructor(private api: ApiService) { }
 
   ngOnInit(): void {
     this.api.getJackpots$().subscribe(jackpots => this.jackpots = jackpots)
+    this.api.getAllCards$().subscribe(games => this.games = games)
+
+    for (let i = 0; i < this.jackpots.length; i++) {
+      const jackpot = this.jackpots[i];
+      for (let j = 0; j < this.games.length; j++) {
+        const game = this.games[j];
+        if (jackpot.game == game.id) {
+          this.list.push({
+            "id": jackpot.game,
+            "name": game.name,
+            "amount": jackpot.amount,
+          })
+        }
+      }
+    }
   }
 
 }
